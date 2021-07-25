f1143871acc4ca5f4d5f35db404c156be8d8bee2
Austin Huang <im@austinhuang.me>
2021-07-25 11:05:23 -0400

    close #1753

--- app/src/main/java/awais/instagrabber/repositories/responses/stories/StoryCta.kt
+++ app/src/main/java/awais/instagrabber/repositories/responses/stories/StoryCta.kt
@@ -1,10 +1,12 @@
 package awais.instagrabber.repositories.responses.stories
 
 import java.io.Serializable
+import com.google.gson.annotations.SerializedName
 import awais.instagrabber.repositories.responses.Hashtag
 import awais.instagrabber.repositories.responses.Location
 import awais.instagrabber.repositories.responses.User
 
 data class StoryCta(
+    @SerializedName("webUri")
     val webUri: String?
 ) : Serializable
